81a1c230e78a0d4f2cf2b9e503031be0b9298721
krdowns <krjdowns16@gmail.com>
2019-01-17 21:36:54 -0800

    working on hamburger placement

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
 	$("body").on('click', '.top', function() {
 		$("nav.menu").toggleClass("menu-show");
 	});
+
+	$("nav").find("a").click(function(e) {
+		e.preventDefault();
+		var section = $(this).attr("href");
+		$("html, body").animate({
+			scrollTop: $(section).offset().top
+		});
+		$("nav.menu").toggleClass("menu-show");
+	});
 });
