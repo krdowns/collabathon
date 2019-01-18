836008b5e3fd4edff13b43dcbba88be79c029db5
jbortiz0623 <jbortiz0623@gmail.com>
2019-01-18 13:27:06 -0800

    push to merge

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+	console.log("App.js running")
 	$("body").on('click', '.top', function() {
 		$("nav.menu").toggleClass("menu-show");
 	});
+
+	// $('#myModal').on('shown.bs.modal', function () {
+	// 	$('#myInput').trigger('focus')
+	// })
 });
