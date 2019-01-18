f9491e6d7a54f0a79b108a4e916c161936c86ce7
krdowns <krjdowns16@gmail.com>
2019-01-18 12:48:57 -0800

    images

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
 
 	$("nav").find("a").click(function(e) {
 		e.preventDefault();
-		console.log("clicked");
 		var section = $(this).attr("href");
 		$("html, body").animate({
 			scrollTop: $(section).offset().top
